46cb2ce05a7018280c2c8151c6782a923f901a35
Brian Groenke <bgroe8@gmail.com>
2021-04-02 19:47:10 +0200

    Refactor blm to allow arbitrary inference algorithms

--- src/BayesModels.jl
+++ src/BayesModels.jl
@@ -2,14 +2,11 @@ module BayesModels
 
 using Turing
 using Statistics
-using Reexport
 using LinearAlgebra
 using DataFrames
-using Optim
-import DynamicPPL
+using Reexport
+import MCMCChains
 @reexport using StatsModels
-
-export blm
 
 function get_nlogp(model)
     # Set up the model call, sample from the prior.
@@ -26,66 +23,6 @@ function get_nlogp(model)
     return nlogp
 end
 
-
-function blm(f::FormulaTerm, df::DataFrame, N::Int=1000, alpha_prior=nothing, beta_prior=nothing, sigma_prior=nothing)
-    f = apply_schema(f, schema(f, df))
-
-    # Define the default value for x when missing
-    y, X = modelcols(f, df)
-    K = size(X, 2)
-    defaults = (y = Vector{Real}(undef, 2),priors=zeros(K))
-    data = (y = y,
-            x = X)
-
-	if isnothing(alpha_prior)
-		alpha_prior = Normal()
-	end
-
-	if isnothing(beta_prior)
-		beta_prior = MvNormal(K, 1)
-	end
-
-	if isnothing(sigma_prior)
-		sigma_prior = InverseGamma(2,3)
-	end
-	
-	@model function bayesreg(
-		x, 
-		y, 
-		alpha_prior, 
-		beta_prior, 
-		sigma_prior,
-		::Type{TV}=Vector{Float64}
-	) where {TV}
-		N, K = size(x)
-
-		sigma ~ sigma_prior
-		intercept ~ alpha_prior
-		beta = TV(undef, K)
-		beta ~ beta_prior
-
-		# beta is K x 1
-		# y is N x 1
-		# x is N x K
-		yhat = intercept .+ x * beta
-
-		y ~ MvNormal(yhat, sigma)	
-	end
-
-	# Model
-	model = bayesreg(X, y, alpha_prior, beta_prior, sigma_prior)
-
-    # Sample
-    chain = sample(model, NUTS(), N)
-    
-    # Get formula names.
-    nms = coefnames(f.rhs)
-    nms_dict = Dict(["beta[$i]" => nms[i] for i in eachindex(nms)])
-
-    # Overwrite the default names.
-    chain = set_names(chain, nms_dict, sorted=false)
-
-    return chain
-end
+include("blm.jl")
 
 end # module
---
+++ src/blm.jl
@@ -0,0 +1,87 @@
+abstract type InferenceStrategy end
+struct MCMC{TAlg,TArgs,TKwArgs} <: InferenceStrategy
+    nsamples::Int
+    alg::TAlg
+    algargs::TArgs
+    algkwargs::TKwArgs
+    MCMC(alg::TAlg, nsamples::Int, args...;kwargs...) where {TAlg<:Turing.InferenceAlgorithm} =
+        new{TAlg,typeof(args),typeof(kwargs)}(nsamples,alg,args,kwargs)
+end
+# TODO add VI support
+# struct VI <: InferenceStrategy end
+
+export InferenceStrategy, MCMC
+
+# Default interface methods
+"""
+    inference(::DynamicPPL.Model, strat::InferenceStrategy)
+
+Run probabilistic inference for the given model using the given InferenceStrategy (e.g. MCMC or variational inference).
+The type of the result depends on the implementation. MCMC implementations should return a ChainDataFrame, whereas VI
+should return a fitted variational distribution.
+"""
+inference(::DynamicPPL.Model, strat::InferenceStrategy) = error("no inference implementation for $(typeof(strat))")
+"""
+    preprocess(::InferenceStrategy, f::FormulaTerm, df::DataFrame)
+
+Applies any necessary preprocessing to the user-provided formula and DataFrame. Can be overrided for specific
+inference strategies if necessary. Should return a data tuple (y,X) representing the response and design matrix respectively.
+"""
+preprocess(::InferenceStrategy, f::FormulaTerm, df::DataFrame) = modelcols(f, df)
+"""
+    postprocess(::InferenceStrategy, inferenceresult, f::FormulaTerm, df::DataFrame)
+
+Applies postprocessing to the given inference result. Returns the (possibly modified) inference result.
+"""
+postprocess(::InferenceStrategy, inferenceresult, f::FormulaTerm, df::DataFrame) = inferenceresult
+
+# MCMC inference
+inference(model::DynamicPPL.Model, mcmc::MCMC) = sample(model, mcmc.alg, mcmc.nsamples, mcmc.algargs...;mcmc.algkwargs...)
+
+function postprocess(::MCMC, chain::MCMCChains.Chains, f::FormulaTerm, df::DataFrame)
+    # Get formula names.
+    nms = coefnames(f.rhs)
+    nms_dict = Dict(["β[$i]" => "β[$(nms[i])]" for i in eachindex(nms)])
+    # Overwrite the default names.
+    chain = MCMCChains.replacenames(chain, nms_dict)
+end
+
+"""
+    blm(f::FormulaTerm, df::DataFrame, inferencestrat::InferenceStrategy=MCMC(NUTS(),1000); alphaprior, betaprior, sigmaprior)
+
+Fits a Bayesian linear model specified by `f` to the given data. Return value depends on the specified inference strategy.
+MCMC will return a ChainDataFrame, whereas VI will return a fitted variational distribution.
+"""
+function blm(f::FormulaTerm, df::DataFrame, inferencestrat::InferenceStrategy=MCMC(NUTS(),1000);
+    alphaprior=nothing, betaprior=nothing, sigmaprior=nothing)
+    F = apply_schema(f, schema(f, df))
+    y, X = preprocess(inferencestrat, F, df)
+    # set priors
+	alphaprior = isnothing(alphaprior) ? Normal() : alphaprior
+	betaprior = isnothing(betaprior) ? MvNormal(zeros(size(X,2)), 1) : betaprior
+	sigmaprior = isnothing(sigmaprior) ? InverseGamma(2,3) : sigmaprior
+	@model function bayesreg(
+		x, 
+		y, 
+		alphaprior, 
+		betaprior, 
+		sigmaprior,
+	)
+        N,K = size(x)
+		σ ~ sigmaprior
+		α ~ alphaprior
+		β ~ betaprior
+		# beta is K x 1
+		# y is N x 1
+		# x is N x K
+		ŷ = α .+ x*β
+		y ~ MvNormal(ŷ, σ)
+	end
+	# Model
+	model = bayesreg(X, y, alphaprior, betaprior, sigmaprior)
+    # Sample
+    res = inference(model, inferencestrat)
+    res = postprocess(inferencestrat,res,F,df)
+end
+
+export blm
